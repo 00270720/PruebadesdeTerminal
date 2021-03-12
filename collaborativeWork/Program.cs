a257a5fdc8014362d45ab85466f95ef3e3258d1c
FernandoCarpio27 <00270720@uca.edu.sv>
2021-03-12 17:07:57 -0600

    First push

---
+++ collaborativeWork/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace collaborativeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite un número: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num < 0)
+                Console.WriteLine("El número no es positivo, intente de nuevo");
+            else if (num % 2 == 0)
+                Console.WriteLine("El número es par");
+            else
+                Console.WriteLine("El número es impar");
+            
+            
+        }
+    }
+}
